1c4ee8c5764a3df7f62415123f37fabc923565e5
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2019-07-16 23:05:14 +0700

    adding view product

--- dataProduct.php
+++ dataProduct.php
@@ -6,7 +6,16 @@ $data = [
 		'category' => 'Limited Edition',
 		'price' => 'IDR 529,000.00',
 		'cover' => '1.jpg',
-		'description' => 'Leather Card Holder'
+		'description' => 'Leather Card Holder, 2 Card Slots, Money Slot, Ferma Seahorse Logo Debossed, Painted Edge, Edge Stitching.',
+		'material' => 'Horween Bison Hide, Limited Edition, Comes with Ferma Zero Waste Packaging.',
+		'images' => [
+			['id' => '1', 'image' => '1.jpg'],
+			['id' => '2', 'image' => '2.jpg'],
+			['id' => '3', 'image' => '3.jpg'],
+			['id' => '4', 'image' => '4.jpg'],
+			['id' => '5', 'image' => '5.jpg'],
+			['id' => '6', 'image' => '6.jpg']
+		]
 	],
 	[
 		'id' => '1',
@@ -14,6 +23,14 @@ $data = [
 		'category' => 'Bag',
 		'price' => 'IDR 260,000.00',
 		'cover' => '1.jpg',
+		'description' => 'Dimension: L 27cm x W 6.5 cm x H 13.5 cm 2,3 L',
+		'material' => 'Polyester Canvas, YKK Zipper, woven label, cotton webbing ',
+		'construction' => 'machine sewed',
+		'images' => [
+			['id' => '1', 'image' => '1.jpg'],
+			['id' => '2', 'image' => '2.jpg'],
+			['id' => '3', 'image' => '3.jpg']
+		]
 	],
 	[
 		'id' => '2',
@@ -21,6 +38,16 @@ $data = [
 		'category' => 'Bag',
 		'price' => 'IDR 1,500,000.00',
 		'cover' => '1.jpg',
+		'description' => 'Product Spesification: 3 compartments, Ferma Winged Seahorse Debossed Logo, Ferma Signature Stitch',
+		'material' => 'Finest Imported Crazy Horse Leather',
+		'images' => [
+			['id' => '1', 'image' => '1.jpg'],
+			['id' => '2', 'image' => '2.jpg'],
+			['id' => '3', 'image' => '3.jpg'],
+			['id' => '4', 'image' => '4.jpg'],
+			['id' => '5', 'image' => '5.jpg'],
+			['id' => '6', 'image' => '6.jpg']
+		]
 	],
 	[
 		'id' => '3',
@@ -28,6 +55,13 @@ $data = [
 		'category' => 'Bag',
 		'price' => 'IDR 890,000.00',
 		'cover' => '1.jpg',
+		'description' => 'Dimension 29 cm x 18.5cm x 5.5 cm, Product Spesification: Ferma Winged Seahorse Debossed Logo, Ferma Signature Stitch',
+		'material' => 'Finest Imported Crazy Horse Leather',
+		'images' => [
+			['id' => '1', 'image' => '1.jpg'],
+			['id' => '2', 'image' => '2.jpg'],
+			['id' => '3', 'image' => '3.jpg']
+		]
 	],
 	[
 		'id' => '4',
@@ -35,6 +69,14 @@ $data = [
 		'category' => 'Bag',
 		'price' => 'IDR 260,000.00',
 		'cover' => '1.jpg',
+		'description' => 'Dimension: L 27cm x W 6.5 cm x H 13.5 cm 2,3 L',
+		'material' => 'Polyester Canvas, YKK Zipper, woven label, cotton webbing ',
+		'construction' => 'machine sewed',
+		'images' => [
+			['id' => '1', 'image' => '1.jpg'],
+			['id' => '2', 'image' => '2.jpg'],
+			['id' => '3', 'image' => '3.jpg']
+		]
 	],
 	[
 		'id' => '5',
@@ -42,7 +84,16 @@ $data = [
 		'category' => 'Bag',
 		'price' => 'IDR 549,000.00',
 		'cover' => '1.jpg',
-		'description' => 'Taruma is inspired by travelers who pursue their passion on taking picture on their journey as memories which they can cherish it later. Taruma is well-designed by our team, travelers, and photographers for our customers so it can accompany you on journey every time, every where'
+		'description' => 'Taruma is inspired by travelers who pursue their passion on taking picture on their journey as memories which they can cherish it later. Taruma is well-designed by our team, travelers, and photographers for our customers so it can accompany you on journey every time, every where',
+		'material' => 'Vintage Pull up leather, Baby Canvas Inner, YKK Zipper, cotton webbing',
+		'construction' => 'Machine Stitched',
+		'images' => [
+			['id' => '1', 'image' => '1.jpg'],
+			['id' => '2', 'image' => '2.jpg'],
+			['id' => '3', 'image' => '3.jpg'],
+			['id' => '4', 'image' => '4.jpg'],
+			['id' => '5', 'image' => '5.jpg']
+		]
 	],
 	[
 		'id' => '6',
@@ -50,7 +101,12 @@ $data = [
 		'category' => 'Hat',
 		'price' => 'IDR 250,000.00',
 		'cover' => '1.jpg',
-		'description' => 'Ferma Technical Hat for your daily activity'
+		'description' => 'Ferma Technical Hat for your daily activity',
+		'material' => 'Ferma Technical Gear Embroidery, Cotton, Trucker Style, Mesh Venting, Adjustable snapback',
+		'images' => [
+			['id' => '1', 'image' => '1.jpg'],
+			['id' => '2', 'image' => '2.jpg']
+		]
 	],
 	[
 		'id' => '7',
@@ -58,7 +114,15 @@ $data = [
 		'category' => 'Watch',
 		'price' => 'IDR 490,000.00',
 		'cover' => '1.jpeg',
-		'description' => 'Bespoke watch straps for your watch, Made to Order Spesification'
+		'description' => 'Bespoke watch straps for your watch, Made to Order Spesification',
+		'images' => [
+			['id' => '1', 'image' => '1.jpeg'],
+			['id' => '2', 'image' => '2.jpg'],
+			['id' => '3', 'image' => '3.jpg'],
+			['id' => '4', 'image' => '4.jpg'],
+			['id' => '5', 'image' => '5.jpg'],
+			['id' => '6', 'image' => '6.jpg']
+		]
 	],
 	[
 		'id' => '8',
@@ -66,7 +130,11 @@ $data = [
 		'category' => 'Watch',
 		'price' => 'IDR 390,000.00',
 		'cover' => '1.jpg',
-		'description' => 'Pasra is Straight Standard Straps suitable for all watches with standard lug model. We combine the handmade process with finest leather for the pasra straps'
+		'description' => 'Pasra is Straight Standard Straps suitable for all watches with standard lug model. We combine the handmade process with finest leather for the pasra straps',
+		'images' => [
+			['id' => '1', 'image' => '1.jpg'],
+			['id' => '2', 'image' => '2.jpg']
+		]
 	],
 	[
 		'id' => '9',
@@ -74,7 +142,8 @@ $data = [
 		'category' => 'Guitar Straps',
 		'price' => 'IDR 240,000.00',
 		'cover' => '1.jpg',
-		'description' => ''
+		'description' => 'Strap Length: 85cm, Strap Width: 6cm, String Length: 70cm, String Width: 2.5cm',
+		'material' => 'Artificial Leather'
 	],
 	[
 		'id' => '10',
@@ -82,7 +151,8 @@ $data = [
 		'category' => 'Guitar Straps',
 		'price' => 'IDR 240,000.00',
 		'cover' => '1.jpg',
-		'description' => ''
+		'description' => 'Strap Length: 85cm, Strap Width: 6cm, String Length: 70cm, String Width: 2.5cm',
+		'material' => 'Artificial Leather'
 	],
 	[
 		'id' => '11',
@@ -90,7 +160,8 @@ $data = [
 		'category' => 'Guitar Straps',
 		'price' => 'IDR 240,000.00',
 		'cover' => '1.jpg',
-		'description' => ''
+		'description' => 'Strap Length: 85cm, Strap Width: 6cm, String Length: 70cm, String Width: 2.5cm',
+		'material' => 'Non-Leather Material'
 	],
 	[
 		'id' => '12',
@@ -98,7 +169,8 @@ $data = [
 		'category' => 'Guitar Straps',
 		'price' => 'IDR 1,000,000.00',
 		'cover' => '1.jpg',
-		'description' => ''
+		'description' => 'Strap Length: 85cm, Strap Width: 6cm, String Length: 70cm, String Width: 2.5cm',
+		'material' => 'Leather'
 	],
 	[
 		'id' => '13',
@@ -106,7 +178,8 @@ $data = [
 		'category' => 'Guitar Straps',
 		'price' => 'IDR 500,000.00',
 		'cover' => '1.jpg',
-		'description' => ''
+		'description' => 'Strap Length: 85cm, Strap Width: 6cm, String Length: 70cm, String Width: 2.5cm',
+		'material' => 'Guitar Strap'
 	],
 	[
 		'id' => '14',
@@ -114,7 +187,12 @@ $data = [
 		'category' => 'Camera Straps',
 		'price' => 'IDR 240,000.00',
 		'cover' => '1.jpg',
-		'description' => ''
+		'description' => 'Finish:  Black, Neck Holder Width: 2.7 cm, String Width: 1,2 cm, Leather Thickness: 1.8 mm, Total Length: 120 cm, Suitable for: Analog, Mirrorless, DSLR',
+		'material' => 'Webbing Strap with String Leather',
+		'images' => [
+			['id' => '1', 'image' => '1.jpg'],
+			['id' => '2', 'image' => '2.jpg']
+		]
 	]
 ];
 ?>
--- product.php
+++ product.php
@@ -35,23 +35,121 @@
 						Product
 					</h1>
 				</div>
-				<div class="margin-top-30px display-flex">
+				<?php if (isset($_GET['id'])) { ?>
 					<?php foreach ($data as $dt) { ?>
-						<div class="df-card app-card">
-							<div 
-								class="image image-full"
-								style="background-image: url('assets/img/product/<?php echo $dt["id"]; ?>/<?php echo $dt["cover"]; ?>');"></div>
-							<div class="margin-5px txt-site txt-18 txt-bold">
-								<?php echo $dt['title']; ?>
+						<?php if ($_GET['id'] == $dt['id']) { ?>
+							<div class="body-content" style="width: 1000px; margin: auto;">
+								<div class="app-card ">
+									<div class="app-product">
+										<div class="col-1">
+											<?php if (count($dt['images']) > 0) {?>
+												<div class="product-image" id="banner">
+													<?php foreach ($dt['images'] as $img) { ?>
+														<div class="image image-full" style="background-image: url('assets/img/product/<?php echo $dt["id"]; ?>/<?php echo $img["image"]; ?>');"></div>
+													<?php } ?>
+												</div>
+											<?php } else { ?>
+												<div 
+													class="image image-full"
+													style="background-image: url('assets/img/product/<?php echo $dt["id"]; ?>/<?php echo $dt["cover"]; ?>');"></div>
+											<?php } ?>
+										</div>
+										<div class="col-2">
+											<div class="margin-5px txt-site txt-16 txt-bold">
+												<h3><?php echo $dt['title']; ?></h3>
+											</div>
+											<table>
+												<tr>
+													<td>
+														Barang
+													</td>
+													<td>:</td>
+													<td>
+														<?php echo $dt['title']; ?>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														Harga
+													</td>
+													<td>:</td>
+													<td>
+														<?php echo $dt['price']; ?>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														Kategori
+													</td>
+													<td>:</td>
+													<td>
+														<?php echo $dt['category']; ?>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														Material
+													</td>
+													<td>:</td>
+													<td>
+														<?php echo $dt['material']; ?>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														Pembuatan
+													</td>
+													<td>:</td>
+													<td>
+														<?php echo $dt['construction']; ?>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														Deskripsi
+													</td>
+													<td>:</td>
+													<td>
+														<?php echo $dt['description']; ?>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														Gambar lainnya
+													</td>
+													<td>:</td>
+													<td>
+														<?php echo count($dt['images']); ?> gambar
+													</td>
+												</tr>
+											</table>
+										</div>
+									</div>
+								</div>
 							</div>
-							<div class="txt-site txt-12 txt-thin">
-								<?php echo $dt['category']; ?>
-								<i class="fa fa-1x fa-circle" style="position: relative; top: -3px; font-size: 4pt;"></i>
-								<?php echo $dt['price']; ?>
-							</div>
-						</div>
+						<?php } ?>
 					<?php } ?>
-				</div>
+				<?php } else { ?>
+					<div class="margin-top-30px display-flex">
+						<?php foreach ($data as $dt) { ?>
+							<div class="df-card app-card">
+								<a href="product.php?path=product&id=<?php echo $dt['id']; ?>">
+									<div 
+										class="image image-full"
+										style="background-image: url('assets/img/product/<?php echo $dt["id"]; ?>/<?php echo $dt["cover"]; ?>');"></div>
+								</a>
+								<div class="margin-5px txt-site txt-14 txt-bold">
+									<?php echo $dt['title']; ?>
+								</div>
+								<div class="txt-site txt-11 txt-thin">
+									<?php echo $dt['category']; ?>
+									<i class="fa fa-1x fa-circle" style="position: relative; top: -3px; font-size: 4pt;"></i>
+									<?php echo $dt['price']; ?>
+								</div>
+							</div>
+						<?php } ?>
+					</div>
+				<?php } ?>
 			</div>
 		</div>
 		<?php include 'layout/footer.php'; ?>
